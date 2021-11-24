00458e9b27a0c10717044d176701d727d69f28ce
sadmanfatin <sadmanfatin208@gmail.com>
2021-11-24 18:07:58 +0600

    commit on 24 nov 2021

---
+++ ViewController/public_html/view.js
@@ -0,0 +1,43 @@
+ function mouseOverTableT2(evt){
+    
+    
+      // t1.setProperty("scrollLeft",100);
+        var  t1scroller = document.getElementById("t1::hscroller") ;
+       var  t2scroller = document.getElementById("t2::hscroller") ;
+ 
+
+   //   document.getElementById(t2scroller).scrollLeft = 200 ;
+     // console.log('t1scroller.scrollLeft :' +t1scroller.scrollLeft);
+     // console.log('t2scroller.scrollLeft :' +t2scroller.scrollLeft);
+      
+//      console.log('vvv'+t1scroller.scrollLeft);
+//      console.log('vvv'+t2scroller.scrollLeft);
+          console.log(345);
+          
+//      console.log('vvv'+t2scroller.scrollLeft);
+     // console.log(t2scroller);
+    // t2scroller.scrollLeft = 500;
+           //t2scroller.scroll
+      
+     }
+
+    function mouseOverTabletT1(evt){
+    
+     
+
+      // t1.setProperty("scrollLeft",100);
+        var  t1scroller = document.getElementById("t1::hscroller") ;
+       var  t2scroller = document.getElementById("t2::hscroller") ;
+ 
+
+   //   document.getElementById(t2scroller).scrollLeft = 200 ;
+     // console.log('t1scroller.scrollLeft :' +t1scroller.scrollLeft);
+     // console.log('t2scroller.scrollLeft :' +t2scroller.scrollLeft);
+      
+      console.log('vvv'+t1scroller.scrollLeft);
+      console.log('vvv'+t2scroller.scrollLeft);
+     // console.log(t2scroller);
+    // t2scroller.scrollLeft = 500;
+           //t2scroller.scroll
+      
+     }
