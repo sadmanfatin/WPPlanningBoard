f7d36eba0885ce32e0aa2bf7e7ce49afcdeb6786
sadmanfatin <sadmanfatin208@gmail.com>
2021-11-25 17:49:37 +0600

    Left Columns freeze policy added

--- ViewController/public_html/view.js
+++ ViewController/public_html/view.js
@@ -1,43 +1,18 @@
  function mouseOverTableT2(evt){
-    
-    
-      // t1.setProperty("scrollLeft",100);
-        var  t1scroller = document.getElementById("t1::hscroller") ;
+        
+       var  t1scroller = document.getElementById("t1::scroller") ;
        var  t2scroller = document.getElementById("t2::hscroller") ;
+       t1scroller.scrollLeft = t2scroller.scrollLeft;
+//     console.log('t1scroller.scrollLeft :' +t1scroller.scrollLeft);
+//      console.log('t2scroller.scrollLeft :' +t2scroller.scrollLeft);
  
-
-   //   document.getElementById(t2scroller).scrollLeft = 200 ;
-     // console.log('t1scroller.scrollLeft :' +t1scroller.scrollLeft);
-     // console.log('t2scroller.scrollLeft :' +t2scroller.scrollLeft);
-      
-//      console.log('vvv'+t1scroller.scrollLeft);
-//      console.log('vvv'+t2scroller.scrollLeft);
-          console.log(345);
-          
-//      console.log('vvv'+t2scroller.scrollLeft);
-     // console.log(t2scroller);
-    // t2scroller.scrollLeft = 500;
-           //t2scroller.scroll
-      
      }
 
-    function mouseOverTabletT1(evt){
-    
-     
-
-      // t1.setProperty("scrollLeft",100);
-        var  t1scroller = document.getElementById("t1::hscroller") ;
+function mouseOverTabletT1(evt){
+               // t1.setProperty("scrollLeft",100);
+       var  t1scroller = document.getElementById("t1::scroller") ;
        var  t2scroller = document.getElementById("t2::hscroller") ;
- 
 
-   //   document.getElementById(t2scroller).scrollLeft = 200 ;
-     // console.log('t1scroller.scrollLeft :' +t1scroller.scrollLeft);
-     // console.log('t2scroller.scrollLeft :' +t2scroller.scrollLeft);
+        t2scroller.scrollLeft = t1scroller.scrollLeft;
       
-      console.log('vvv'+t1scroller.scrollLeft);
-      console.log('vvv'+t2scroller.scrollLeft);
-     // console.log(t2scroller);
-    // t2scroller.scrollLeft = 500;
-           //t2scroller.scroll
-      
-     }
+}
