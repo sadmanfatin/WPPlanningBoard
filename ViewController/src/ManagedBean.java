e4b426e42497678a015e5925f6767d4417655a7b
sadmanfatin <sadmanfatin208@gmail.com>
2022-05-11 16:00:17 +0600

    Update ManagedBean.java

--- ViewController/src/ManagedBean.java
+++ ViewController/src/ManagedBean.java
@@ -479,7 +479,7 @@ public class ManagedBean {
         appM.getWpPlanningBoardLoadVO1().executeQuery();
       //  this.refreshQueryKeepingCurrentRow(appM.getWpPlanningBoardVO1());
         AdfFacesContext.getCurrentInstance().addPartialTarget(this.getPlanningBoardLoadTable());
-      //  AdfFacesContext.getCurrentInstance().addPartialTarget(this.getPlanningBoardTable());
+        AdfFacesContext.getCurrentInstance().addPartialTarget(this.getPlanningBoardTable());
         
     }
     
